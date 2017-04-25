8778a16fa41a4e6574160505606ddd213a77c427
Michael Sanfelippo <mj.sanfelippo@gmail.com>
2017-04-25 13:00:40 -0700

    Put more test data
    Made some more random test data to be inserted into the database

--- Create Tables and Insert Test Data.sql
+++ Create Tables and Insert Test Data.sql
@@ -127,6 +127,7 @@ INSERT INTO stakeholder VALUES (1, 'Erika', 'Perez'), (2, 'Abel', 'Cole'), (3, '
 
 INSERT INTO project VALUES (1, '2017-04-22', '2018-05-23', 'CECS 323 Database Project');
 
+
 INSERT INTO employee_project VALUES (1, 1, '2017-04-22', 0), (1, 2, '2017-04-22', 0);
 
 INSERT INTO stakeholder_project VALUES (1,1,5, 'User');
@@ -146,3 +147,48 @@ INSERT INTO task VALUES (1, 'Design the database');
 
 
 INSERT INTO project_backlog VALUES (1, 1, 1, 'Create and populate the database', '2017-04-22');
+
+INSERT INTO project VALUES (2, '2017-04-25', '2019-03-23', 'CECS 277 Java Project');
+INSERT INTO project VALUES (3, '2016-03-05', '2020-09-11', 'CECS 201 Engineering Project');
+
+INSERT INTO employee_project VALUES (2, 5, '2017-04-25', 5);
+INSERT INTO employee_project VALUES (2,6,'2017-04-25', 10);
+INSERT INTO employee_project VALUES (2, 7, '2017-04-25', 0);
+INSERT INTO employee_project VALUES (2, 9, '2017-04-25', 30);
+INSERT INTO employee_project VALUES (3, 10, '2017-04-23', 0);
+INSERT INTO employee_project VALUES (3, 12,'2016-05-03', 100);
+INSERT INTO employee_project VALUES (3, 15, '2016-07-23', 30);
+
+INSERT INTO stakeholder_project VALUES (2, 2, 10, 'Investor');
+INSERT INTO stakeholder_project VALUES (3,3,3,'Advisor');
+INSERT INTO stakeholder_project VALUES (4,3,6, 'User');
+
+INSERT INTO scrum_team VALUES (3, 'Cloud 9');
+INSERT INTO scrum_team VALUES (4, 'Phoenix1');
+
+INSERT INTO employee_scrumTeam VALUES (5, 2, FALSE);
+INSERT INTO employee_scrumTeam VALUES (6, 2, FALSE);
+INSERT INTO employee_scrumTeam VALUES (7, 3, TRUE);
+INSERT INTO employee_scrumTeam VALUES (8, 3, FALSE);
+INSERT INTO employee_scrumTeam VALUES (9, 4, TRUE);
+INSERT INTO employee_scrumTeam VALUES (10, 4, FALSE);
+
+INSERT INTO userStory VALUES (2, 'Engineer', 'build something', 'people can live in it', 'Construction');
+INSERT INTO userStory VALUES (3, 'Investor', 'make money', 'I become richer', 'Self-interest');
+INSERT INTO userStory VALUES (4, 'Developer', 'make a good interface', 'people will buy it', 'GUI');
+INSERT INTO userStory VALUES (5, 'Manager', 'manage people', 'I will be respected', 'Self-interest');
+
+INSERT INTO sprint_backlog VALUES (2, 2, 5, 'In progress', 3);
+INSERT INTO sprint_backlog VALUES (2, 3, 6, 'Completed', 1);
+INSERT INTO sprint_backlog VALUES (3, 4, 7, 'Almost done', 8);
+
+INSERT INTO sprint VALUES (5, 2, TRUE, '2017-04-25', '2017-05-26', 2);
+INSERT INTO sprint VALUES (6, 2, FALSE, '2017-05-25', '2017-06-26', 2);
+
+INSERT INTO task VALUES (2, 'Build something');
+INSERT INTO task VALUES (2, 'Build another thing');
+INSERT INTO task VALUES (3, 'Make more money');
+INSERT INTO task VALUES (4, 'Build the gui');
+INSERT INTO task VALUES (4, 'Design the gui');
+INSERT INTO task VALUES (5, 'Yell at employees');
+INSERT INTO task VALUES (5, 'Yell at more employees');
